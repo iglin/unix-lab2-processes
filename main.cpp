728f0b626fe78a13591d8766214192c0101bf3ba
iglin <iglin@github.com>
2016-12-18 00:46:10 +0400

    one-side socket

--- main.cpp
+++ main.cpp
@@ -9,8 +9,14 @@
 #include <fcntl.h>
 #include <iostream>
 #include <cmath>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
 
 using namespace std;
+
+#define BUFSIZE 256
+#define PORT 6969
 
 pid_t p, q;
 
@@ -19,6 +25,29 @@ sem_t empty, full;
 key_t shmkey;                 /*      shared memory key       */
 int shmid;                    /*      shared memory id        */
 sem_t *sem, *sem2;
+
+int sock;
+struct sockaddr_in addr;
+int bytes_read, total = 0;
+
+int main3() {
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+    char buf[BUFSIZE];
+    while(1) {
+        int socket = accept(sock, (struct sockaddr *)&addr, (socklen_t *) sizeof(addr));
+        bytes_read = recvfrom(sock, buf, BUFSIZE, 0, NULL, NULL);
+        total = total + bytes_read;
+        buf[bytes_read] = '\0';
+        fprintf(stdout, buf);fflush(stdout);
+        if (total>BUFSIZE) break;
+    }
+    printf("\nTotal %d bytes received", total);
+    return 0;
+}
 
 double calculate(char* par) {
     string input = string(par);
@@ -47,14 +76,14 @@ double calculate(char* par) {
     }
 }
 
-int main() {
+int main22() {
     char *s = (char *) "+;121;-55.7";
 
     printf("result %f\n", calculate(s));
     return 0;
 }
 
-int main22() {
+int main() {
     char *e[]={"",""};
 
     /* initialize a shared variable in shared memory */
@@ -84,13 +113,37 @@ int main22() {
         /* Родитель получает PID ребенка*/
         printf("Parent proc...\n");
         //wait(&q); /* Ждать завершения потомков */
-        while(1) {
-            sem_wait(sem);
-            printf("parent!\n");
-            sleep(5);
-            sem_post(sem2);
-        }
+        sem_wait(sem);
+        double a, b;
+        cout << "Input a: " << endl;
+        cin >> a;
+        cout << "Input b: " << endl;
+        cin >> b;
 
+        string s = "s;" + to_string(a);
+        sem_post(sem2);
+
+        //send
+
+        sock = socket(AF_INET, SOCK_DGRAM, 0);
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(PORT);
+        addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+        cout << "parent socket: " << sock << endl;
+        int ret = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+        cout << "parent connected: " << ret << endl;
+        send(sock, s.c_str(), strlen(s.c_str()), 0);
+        close(sock);
+
+        sem_wait(sem);
+
+        //recieve
+
+      //  cout << "sqr(a) = " << result << endl;
+
+        sleep(5);
+       // sem_post(sem2);
+        kill(p, 9);
         printf("Parent ends");
         //shmdt (p);
         shmctl (shmid, IPC_RMID, 0);
@@ -103,10 +156,27 @@ int main22() {
     {
         //   execv("./hello", e); /* Ребенок замещает себя другой программой*/
         printf("Kid proc...\n");
-        while(1) {
+        sock = socket(AF_INET, SOCK_DGRAM, 0);
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(PORT);
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+        char buf[BUFSIZE];
+        while (1) {
             sem_wait(sem2);
-            printf("kid!\n");
-            sleep(5);
+
+            cout << "server in while" << endl;
+           // int socket = accept(sock, (struct sockaddr *)&addr, (socklen_t *) sizeof(addr));
+            //cout << "accepted: " << socket << endl;
+            bytes_read = recvfrom(sock, buf, BUFSIZE, 0, NULL, NULL);
+            cout << "read: " << socket << endl;
+            total = total + bytes_read;
+            buf[bytes_read] = '\0';
+           // fprintf(stdout, buf);fflush(stdout);
+         //   if (total>BUFSIZE) break;
+            cout << "buf: " << buf << endl;
+            printf("\nTotal %d bytes received\n", total);
+            cout << "res: " << calculate(buf) << endl;
             sem_post(sem);
         }
         printf("Kid ends");
